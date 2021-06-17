b0e2b99f133ff9fe4add78aa63ff9dba6d73d10d
icegps_cky <chenkaiyang@icegps.com.cn>
2021-06-17 19:18:01 +0800

    先搁置

--- app/src/main/java/com/example/automaticopenhotspot/MainActivity.java
+++ app/src/main/java/com/example/automaticopenhotspot/MainActivity.java
@@ -3,13 +3,35 @@ package com.example.automaticopenhotspot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG = "MainActivity";
+    private Button main_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello,world");
+        initView();
+    }
+
+    private void initView() {
+        main_btn =findViewById(R.id.main_btn);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.main_btn:
+                Log.d(TAG, "onClick: ");
+                //搁置
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/automaticopenhotspot/WIFIInformation.java
@@ -0,0 +1,12 @@
+package com.example.automaticopenhotspot;
+
+/**
+ * @author hello word
+ * @desc 热点的信息设置
+ * @date 2021/6/17
+ */
+public class WIFIInformation {
+    public final static String END = "end";
+    public final static String HOST_SPOT_SSID = "wifi_socket";
+    public final static String HOST_SPOT_WORD = "123456789";
+}
